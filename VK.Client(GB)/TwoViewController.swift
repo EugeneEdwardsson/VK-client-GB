21a748fd4156b2a773ff8765978b995eb5f26e51
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-04-09 15:41:25 +0300

    Add. Test Project

---
+++ VK.Client(GB)/TwoViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TwoViewController.swift
+//  VK.Client(GB)
+//
+//  Created by Eugene Berezkin on 08.04.2022.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tapScreen = UITapGestureRecognizer(target: self, action: #selector(hideScreen))
+        view.addGestureRecognizer(tapScreen)
+        
+    }
+    
+    @objc func hideScreen() {
+        view.endEditing(true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(willShowKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        
+        
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(willHideKeyboard(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+
+   @objc func willShowKeyboard(_ notification: NotificationCenter) {
+       
+       print("ShowKeyboard - \(#function)")
+        
+    }
+    
+  
+    @objc func willHideKeyboard(_ notification: NotificationCenter) {
+        
+        print("HideKeyboard - \(#function)")
+    }
+    
+    
+}
