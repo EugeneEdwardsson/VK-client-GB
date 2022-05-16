aee58849c7c5d51e80453a74f7990b4602fff87c
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-05-16 17:40:02 +0300

    Add group Model

---
+++ VK.Client(GB)/Model/Group.swift
@@ -0,0 +1,15 @@
+//
+//  Group.swift
+//  VK.Client(GB)
+//
+//  Created by Eugene Berezkin on 16.05.2022.
+//
+
+import UIKit
+
+
+struct Group {
+    
+    var title = String()
+    var avatar = UIImage()
+}
---
+++ VK.Client(GB)/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  VK.Client(GB)
+//
+//  Created by Eugene Berezkin on 16.05.2022.
+//
+
+import UIKit
+
+
+
+struct Friend {
+    
+    var name = String()
+    var avatar = [UIImage]()
+    var photos = [UIImage]()
+}
