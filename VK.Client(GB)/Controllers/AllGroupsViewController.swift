673ea1d7729df99ffdaa08e9f64d64ebce2e95ef
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-07-02 19:56:08 +0300

    Add changes

--- VK.Client(GB)/Controllers/AllGroupsViewController.swift
+++ VK.Client(GB)/Controllers/AllGroupsViewController.swift
@@ -22,21 +22,37 @@ class AllGroupsViewController: UIViewController {
     
     func fillGroups(){
         
-        let groupOne = Group(title: "Apple presentation", avatar: UIImage(systemName: "applelogo"))
-        let groupTwo = Group(title: "National Geographic", avatar: UIImage(systemName: "globe"))
-        let groupThree = Group(title: "RBK", avatar: UIImage(systemName: "dollarsign.circle"))
-        let groupFour = Group(title: "Sci-Fi Movies", avatar: UIImage(systemName: "film"))
-        let groupFive = Group(title: "Rock Music", avatar: UIImage(systemName: "guitars"))
-        let groupSix = Group(title: "Wylsa", avatar: UIImage(systemName: "iphone.circle"))
-        let groupSeven = Group(title: "News vlog", avatar: UIImage(systemName: "newspaper.fill"))
-        let groupEight = Group(title: "Crazy Kitchen", avatar: UIImage(systemName: "fork.knife.circle"))
-        let groupNine = Group(title: "The minimalist", avatar: UIImage(systemName: "figure.walk.diamond"))
-        let groupTen = Group(title: "LIFE", avatar: UIImage(systemName: "hourglass.circle.fill"))
-        let groupEleven = Group(title: "Swift Developers", avatar: UIImage(named: "swift"))
-        let groupTwelve = Group(title: "Geo History", avatar: UIImage(systemName: "globe.americas"))
-        let groupThreeteen = Group(title: "Adventures", avatar: UIImage(systemName: "airplane.departure"))
-        let groupFourteen = Group(title: "Movies", avatar: UIImage(systemName: "4k.tv"))
-        let groupFiveteen = Group(title: "Reggy Music", avatar: UIImage(systemName: "headphones"))
+        let groupOne = Group(title: "Apple presentation", avatar: UIImage(systemName: "applelogo")!)
+        let groupTwo = Group(title: "National Geographic", avatar: UIImage(systemName: "globe")!)
+        let groupThree = Group(title: "RBK", avatar: UIImage(systemName: "dollarsign.circle")!)
+        let groupFour = Group(title: "Sci-Fi Movies", avatar: UIImage(systemName: "film")!)
+        let groupFive = Group(title: "Rock Music", avatar: UIImage(systemName: "guitars")!)
+        let groupSix = Group(title: "Wylsa", avatar: UIImage(systemName: "iphone.circle")!)
+        let groupSeven = Group(title: "News vlog", avatar: UIImage(systemName: "newspaper.fill")!)
+        let groupEight = Group(title: "Crazy Kitchen", avatar: UIImage(systemName: "fork.knife.circle")!)
+        let groupNine = Group(title: "The minimalist", avatar: UIImage(systemName: "figure.walk.diamond")!)
+        let groupTen = Group(title: "LIFE", avatar: UIImage(systemName: "hourglass.circle.fill")!)
+        let groupEleven = Group(title: "Swift Developers", avatar: UIImage(systemName: "swift")!)
+        let groupTwelve = Group(title: "Geo History", avatar: UIImage(systemName: "globe.americas")!)
+        let groupThreeteen = Group(title: "Adventures", avatar: UIImage(systemName: "airplane.departure")!)
+        let groupFourteen = Group(title: "Movies", avatar: UIImage(systemName: "4k.tv")!)
+        let groupFiveteen = Group(title: "Reggy Music", avatar: UIImage(systemName: "headphones")!)
+        
+        allGroups.append(groupOne)
+        allGroups.append(groupTwo)
+        allGroups.append(groupThree)
+        allGroups.append(groupFour)
+        allGroups.append(groupFive)
+        allGroups.append(groupSix)
+        allGroups.append(groupSeven)
+        allGroups.append(groupEight)
+        allGroups.append(groupNine)
+        allGroups.append(groupTen)
+        allGroups.append(groupEleven)
+        allGroups.append(groupTwelve)
+        allGroups.append(groupThreeteen)
+        allGroups.append(groupFourteen)
+        allGroups.append(groupFiveteen)
     }
     
     
@@ -66,6 +82,7 @@ class AllGroupsViewController: UIViewController {
         
         override func viewDidLoad() {
             super.viewDidLoad()
+            fillGroups()
             tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: reuseIdentifierCustom)
             tableView.delegate = self
             tableView.dataSource = self
@@ -86,13 +103,13 @@ class AllGroupsViewController: UIViewController {
         
         
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return groupArray.count
+            return allGroups.count
         }
         
      
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifierCustom", for: indexPath) as? CustomTableViewCell else {return UITableViewCell()}
-            cell.configure(group: groupArray[indexPath.row])
+            cell.configure(group: allGroups[indexPath.row])
             
             return cell
         }
