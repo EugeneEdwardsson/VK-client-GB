c3c0df3f5afeda714f0e335e2dd30d23c5a317d4
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-07-03 10:40:44 +0300

    Add CollectionView and Controllers

---
+++ VK.Client(GB)/Controllers/PhotoGalleryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoGalleryViewController.swift
+//  VK.Client(GB)
+//
+//  Created by Eugene Berezkin on 03.07.2022.
+//
+
+import UIKit
+
+class PhotoGalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
