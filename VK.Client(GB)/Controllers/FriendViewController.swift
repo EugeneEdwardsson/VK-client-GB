5270e2a87f44572d022321f0edb8f81360893b50
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-07-04 10:54:53 +0300

    Add changes

--- VK.Client(GB)/Controllers/FriendViewController.swift
+++ VK.Client(GB)/Controllers/FriendViewController.swift
@@ -100,18 +100,7 @@ class FriendViewController: UIViewController {
 
 
 
-extension FriendViewController: UISearchBarDelegate {
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.isEmpty {
-            self.friendsArray = self.saveArray
-        } else {
-            self.friendsArray = self.friendsArray.filter({item in item.name.lowercased().contains(searchText.lowercased())})
-        }
-        self.tableView.reloadData()
-    }
-    
-}
+
 
 
     
