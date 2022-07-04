e5b6a78b2a18f318461329dce23d7e08f0dc9623
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-07-04 11:40:07 +0300

    Add changes

--- VK.Client(GB)/Cells/CustomTableViewCell.swift
+++ VK.Client(GB)/Cells/CustomTableViewCell.swift
@@ -42,7 +42,7 @@ class CustomTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        avatarView.layer.cornerRadius = 80/2
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
