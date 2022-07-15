8a5446f4ae61a8f704a201ee6a459ebbd7de9f5e
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-07-15 09:51:16 +0300

    Add viewcontroller

---
+++ VK.Client(GB)/Controllers/AnimateViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AnimateViewController.swift
+//  VK.Client(GB)
+//
+//  Created by Eugene Berezkin on 13.07.2022.
+//
+
+import UIKit
+
+class AnimateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
