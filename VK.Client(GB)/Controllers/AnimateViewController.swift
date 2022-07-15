93a385c1fc1c7044ec8eb00e56c58e40781b0d2f
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2022-07-15 10:30:11 +0300

    Add func

--- VK.Client(GB)/Controllers/AnimateViewController.swift
+++ VK.Client(GB)/Controllers/AnimateViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class AnimateViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstIndicatorView: UIView!
+    @IBOutlet weak var secondIndicatorView: UIView!
+    @IBOutlet weak var thirdIndicatorView: UIView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +23,43 @@ class AnimateViewController: UIViewController {
     }
     
 
+    
+    
+    func roundAnimate() {
+        
+      UIView.animate(withDuration: 2) {[weak self] in
+      self?.firstIndicatorView.alpha = 0
+      self?.secondIndicatorView.alpha = 1
+  } completion: { _ in
+      UIView.animate(withDuration: 2) {[weak self] in
+      self?.secondIndicatorView.alpha = 0
+      self?.thirdIndicatorView.alpha = 1
+  } completion: { _ in
+      UIView.animate(withDuration: 2) {[weak self] in
+      self?.thirdIndicatorView.alpha = 0
+      self?.firstIndicatorView.alpha = 1
+  } completion: { _ in
+      
+     
+     }
+      
+   }
+ 
+}
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        firstIndicatorView.alpha = 1
+        secondIndicatorView.alpha = 0
+        thirdIndicatorView.alpha = 0
+        
+        roundAnimate()
 
 }
+            
+   
+        
+}
+
